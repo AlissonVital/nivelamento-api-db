6d84b194f432e77bad7eb1120871ac2eaa5c582c
Alisson Vital <alisson.costa-terceiro@cea.com.br>
2023-11-15 09:27:18 -0300

    SB 16 Request Params

--- src/main/java/projetos/spring/nivelamento_sb/repository/AnimeRepository.java
+++ src/main/java/projetos/spring/nivelamento_sb/repository/AnimeRepository.java
@@ -3,10 +3,13 @@ package projetos.spring.nivelamento_sb.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import projetos.spring.nivelamento_sb.domain.Anime;
 
+import java.util.List;
+
 
 //ESTA CLASSE SERA A CONEXÃO COM O BANCO DE DADOS
 
 //Long É O ATRIBUTO QUE REPRESENTA O ID
 public interface AnimeRepository extends JpaRepository<Anime, Long> {
 
+    List<Anime> findByName(String name);
 }
