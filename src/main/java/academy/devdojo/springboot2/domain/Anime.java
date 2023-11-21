9891c5f93f7d8d8ed1f137e6bef04f79d03cee2d
Alisson Vital <alisson.costa-terceiro@cea.com.br>
2023-11-21 09:01:01 -0300

    Aula SB 22 Sobrescrevendo handler do Spring

---
+++ src/main/java/academy/devdojo/springboot2/domain/Anime.java
@@ -0,0 +1,24 @@
+package academy.devdojo.springboot2.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//ESTA CLASSE SERA O DOMINIO,
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
